00752153f11d2739f5fded53f7aca6e28672f9ee
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-02-20 16:22:56 +0100

    ElectricalBox

--- CustomStructures/AssetHandlers/ElectricalRoomHandler.cs
+++ CustomStructures/AssetHandlers/ElectricalRoomHandler.cs
@@ -4,11 +4,14 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Exiled.API.Features;
 using InventorySystem.Items.Pickups;
-using MEC;
+using Mistaken.API.Diagnostics;
 using Mistaken.UnityPrefabs;
+using Mistaken.UnityPrefabs.Misc;
 using Mistaken.UnityPrefabs.SegmentDisplay;
 using UnityEngine;
 
@@ -26,8 +29,17 @@ namespace Mistaken.CustomStructures.AssetHandlers
 
             this.triggerItem = Asset.ConnectedItemScriptTriggers.Single(x => x.Value.Name == "EZ_ELECTRICAL_ROOM_TESLA_LEVER").Key;
 
+            this.electricalBox = this.GetComponentInChildren<ElectricalBoxScript>();
+
+            this.teslas = FindObjectsOfType<TeslaGate>();
+            foreach (var tesla in this.teslas)
+                this.StartCoroutine(this.UpdateLight(tesla));
+
             Map.Rooms.First(x => x.Type == Exiled.API.Enums.RoomType.EzCollapsedTunnel)
                 .Color = Color.white;
+
+            Exiled.Events.Handlers.Player.TriggeringTesla += this.Player_TriggeringTesla;
+            Exiled.Events.Handlers.Scp079.InteractingTesla += this.Scp079_InteractingTesla;
         }
 
         public override void OnDestroy()
@@ -38,6 +50,12 @@ namespace Mistaken.CustomStructures.AssetHandlers
         {
             if (this.cooldown)
                 return;
+
+            if(this.lever is null)
+            {
+                Log.Warn("Lever is null");
+                this.SetLever();
+            }
 
             if (name == "EZ_ELECTRICAL_ROOM_TESLA_LEVER")
             {
@@ -66,6 +84,8 @@ namespace Mistaken.CustomStructures.AssetHandlers
         private Animator lever;
         private TimerSegmentScript display;
         private ItemPickupBase triggerItem;
+        private ElectricalBoxScript electricalBox;
+        private TeslaGate[] teslas;
 
         private bool currentState = true;
         private bool cooldown = false;
@@ -75,6 +95,72 @@ namespace Mistaken.CustomStructures.AssetHandlers
             this.lever = this.gameObject.GetComponentInChildren<Animator>();
             this.display = this.gameObject.GetComponentInChildren<TimerSegmentScript>();
             this.display?.SetText("----");
+        }
+
+        private IEnumerator UpdateLight(TeslaGate teslaGate)
+        {
+            int index = this.teslas.IndexOf(teslaGate);
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+
+                if (this.suppresedTeslas.Contains(teslaGate))
+                {
+                    Log.Debug("Suppresed");
+                    continue;
+                }
+
+                if (API.Utilities.Map.TeslaMode == API.Utilities.TeslaMode.ENABLED)
+                {
+                    if (teslaGate.isIdling)
+                        this.electricalBox.SetStatus(index + 1, Color.yellow);
+                    else if (teslaGate.InProgress)
+                        this.electricalBox.SetStatus(index + 1, Color.red);
+                    else
+                        this.electricalBox.SetStatus(index + 1, Color.green);
+                }
+                else
+                {
+                    this.electricalBox.SetStatus(index + 1, new Color(0, 0, 0, 0));
+                    Log.Debug("Disabled");
+                }
+            }
+        }
+
+        private readonly HashSet<TeslaGate> suppresedTeslas = new HashSet<TeslaGate>();
+
+        private void Player_TriggeringTesla(Exiled.Events.EventArgs.TriggeringTeslaEventArgs ev)
+        {
+            if (!ev.IsTriggerable || API.Utilities.Map.TeslaMode != API.Utilities.TeslaMode.ENABLED)
+                return;
+
+            int index = this.teslas.IndexOf(ev.Tesla);
+
+            this.electricalBox.SetStatus(index + 1, Color.red);
+            this.suppresedTeslas.Add(ev.Tesla);
+
+            Module.CallSafeDelayed(1.5f, () =>
+            {
+                this.electricalBox.SetStatus(index + 1, Color.yellow);
+                this.suppresedTeslas.Remove(ev.Tesla);
+            }, "UnsuppressTesla");
+        }
+
+        private void Scp079_InteractingTesla(Exiled.Events.EventArgs.InteractingTeslaEventArgs ev)
+        {
+            if (!ev.IsAllowed || API.Utilities.Map.TeslaMode != API.Utilities.TeslaMode.ENABLED)
+                return;
+
+            int index = this.teslas.IndexOf(ev.Tesla);
+
+            this.electricalBox.SetStatus(index + 1, Color.red);
+            this.suppresedTeslas.Add(ev.Tesla);
+
+            Module.CallSafeDelayed(.75f, () =>
+            {
+                this.electricalBox.SetStatus(index + 1, Color.yellow);
+                this.suppresedTeslas.Remove(ev.Tesla);
+            }, "UnsuppressTesla");
         }
     }
 }
